8471df72192192b6e43ece08fb496c8ce74665c0
Samiya Kazi <samiya.kazi09@gmail.com>
2023-02-17 21:29:38 +0600

    refactor: fixed imports in app routing module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomePageComponent } from './pages/home-page/home-page/home-page.component';
-import { LoginPageComponent } from './pages/login-page/login-page/login-page.component';
-import { RegisterPageComponent } from './pages/register-page/register-page/register-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 const routes: Routes = [
   {path: 'login', component: LoginPageComponent},
